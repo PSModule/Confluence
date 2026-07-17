38317f103a1cf530397964ee1b74f3cc1e95fbd3
Marius Storhaug <marstor@hotmail.com>
2026-07-17 20:48:54 +0200

    Remove manual class dot-sourcing from Confluence variable bootstrap

--- src/variables/private/Confluence.ps1
+++ src/variables/private/Confluence.ps1
@@ -1,27 +1,4 @@
-﻿$classFiles = @(
-    'Base/ConfluenceEntity.ps1'
-    'Spaces/ConfluenceSpace.ps1'
-    'Spaces/ConfluenceSpacePermission.ps1'
-    'Spaces/ConfluenceSpaceProperty.ps1'
-    'Pages/ConfluencePage.ps1'
-    'Pages/ConfluencePageVersion.ps1'
-    'Folders/ConfluenceFolder.ps1'
-    'Comments/ConfluenceComment.ps1'
-    'Labels/ConfluenceLabel.ps1'
-    'ContentProperties/ConfluenceContentProperty.ps1'
-    'Attachments/ConfluenceAttachment.ps1'
-    'Restrictions/ConfluenceRestriction.ps1'
-    'BlogPosts/ConfluenceBlogPost.ps1'
-    'Users/ConfluenceUser.ps1'
-    'Site/ConfluenceSiteInfo.ps1'
-    'Auth/ConfluenceAccessibleResource.ps1'
-)
-
-foreach ($classFile in $classFiles) {
-    . (Join-Path -Path $PSScriptRoot -ChildPath (Join-Path -Path '../../classes/public' -ChildPath $classFile))
-}
-
-$script:Confluence = [pscustomobject]@{
+﻿$script:Confluence = [pscustomobject]@{
     ContextVault  = 'PSModule.Confluence'
     # The API-gateway base is https://api.atlassian.com/ex/confluence/<cloudId>. These are the two
     # Confluence API families that hang off it; the map keeps the version prefixes in one place.
@@ -38,4 +15,3 @@ $script:Confluence = [pscustomobject]@{
     }
     Config        = $null
 }
-
